b8f410f891618e5bab6406b0679521677a0ddb8f
Fr4nc1e <75559690+Fr4nc1e@users.noreply.github.com>
2022-11-27 12:21:09 +0800

    Retrofit Setup

---
+++ app/src/main/java/com/mvvm/newspaper/model/Source.kt
@@ -0,0 +1,6 @@
+package com.mvvm.newspaper.model
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/mvvm/newspaper/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.mvvm.newspaper.util
+
+object Constants {
+
+    const val API_KEY = "c82ce9ec4c8e469dbb759905ece1c837"
+    const val BASE_URL = "https://newsapi.org"
+}
