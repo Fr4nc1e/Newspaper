3188d8e3d3b5657f5e7a521c8e8a2c0922528e22
Fr4nc1e <75559690+Fr4nc1e@users.noreply.github.com>
2022-11-27 11:24:50 +0800

    Complete BottomNavigation

--- app/src/main/java/com/mvvm/newspaper/ui/MainActivity.kt
+++ app/src/main/java/com/mvvm/newspaper/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.mvvm.newspaper.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.mvvm.newspaper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +12,19 @@ class MainActivity : AppCompatActivity() {
 
     private val binding get() = _binding!!
 
+    private val navController by lazy {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(binding.newsNavHostFragment.id) as NavHostFragment
+
+        navHostFragment.navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNavigationView
+            .setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/mvvm/newspaper/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/mvvm/newspaper/ui/fragments/ArticleFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mvvm.newspaper.databinding.FragmentArticleBinding
-import com.mvvm.newspaper.databinding.FragmentBreakingNewsBinding
 
 class ArticleFragment : Fragment() {
 
